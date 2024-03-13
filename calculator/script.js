dc2c8a9c329bfe176b31a5326e6c488ce41ee6f2
Dahnhiel <145332501+Dahnhiel@users.noreply.github.com>
2024-03-14 10:48:03 +1200

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,19 @@
+var display = document.querySelector(".display")
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+
+    catch(error) {
+        display.value = "error"
+    }
+}
